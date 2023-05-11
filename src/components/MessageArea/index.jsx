08c9bf18f1b2a8fafb1192e58f123145209d95e6
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-11 17:20:17 +0300

    Add icon

--- src/components/MessageArea/index.jsx
+++ src/components/MessageArea/index.jsx
@@ -1,27 +1,38 @@
 import React, { useState } from "react";
 import styles from "./MessageArea.module.scss";
+import Icon from "@mdi/react";
+import { mdiSend } from "@mdi/js";
 
 function MessageArea(props) {
-  const [ text, setText ] = useState( "" );
-  const { addMessage } = props;
+	const [text, setText] = useState("");
+	const { addMessage } = props;
 
 	const changeHandler = ({ target: { value } }) => {
 		setText(value);
-  };
-  
-  const submitHandler = ( event ) => {
-    event.preventDefault();
-    addMessage( text );
-  }
+	};
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		text && addMessage(text);
+	};
 
 	return (
-		<form className={styles.container} onSubmit={submitHandler}>
+		<form
+			className={styles.container}
+			onSubmit={submitHandler}>
 			<textarea
 				value={text}
 				onChange={changeHandler}
 				className={styles["text-area"]}
 			/>
-			<button type="submit" className={styles.button}>{"->"}</button>
+			<button
+				type="submit"
+				className={styles.button}>
+				<Icon
+					path={mdiSend}
+					size={1}
+				/>
+			</button>
 		</form>
 	);
 }
